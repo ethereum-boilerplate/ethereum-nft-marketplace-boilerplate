15794be69d8e6e868b6892a9caa82bfeb6c31d6f
IAmJaysWay <91806278+IAmJaysWay@users.noreply.github.com>
2021-12-10 19:24:15 +0200

    Error handling for failed fetch

--- src/hooks/useNFTTokenIds.js
+++ src/hooks/useNFTTokenIds.js
@@ -1,3 +1,4 @@
+import { ContactsOutlined } from "@ant-design/icons";
 import { useMoralisDapp } from "providers/MoralisDappProvider/MoralisDappProvider";
 import { useEffect, useState } from "react";
 import { useMoralisWeb3Api, useMoralisWeb3ApiCall } from "react-moralis";
@@ -8,33 +9,50 @@ export const useNFTTokenIds = (addr) => {
   const { chainId } = useMoralisDapp();
   const { resolveLink } = useIPFS();
   const [NFTTokenIds, setNFTTokenIds] = useState([]);
-  const [totalNFTs, setTotalNFTs] = useState()
+  const [totalNFTs, setTotalNFTs] = useState();
+  const [fetchSuccess, setFetchSuccess] = useState(true);
   const {
     fetch: getNFTTokenIds,
     data,
     error,
     isLoading,
-  } = useMoralisWeb3ApiCall(token.getAllTokenIds, { chain: chainId, address: addr });
+  } = useMoralisWeb3ApiCall(token.getAllTokenIds, {
+    chain: chainId,
+    address: addr,
+    limit: 10,
+  });
 
   useEffect(async () => {
     if (data?.result) {
       const NFTs = data.result;
       setTotalNFTs(data.total);
+      setFetchSuccess(true);
       for (let NFT of NFTs) {
         if (NFT?.metadata) {
           NFT.metadata = JSON.parse(NFT.metadata);
           NFT.image = resolveLink(NFT.metadata?.image);
-        }else if (NFT?.token_uri){
-          await fetch(NFT.token_uri)
-          .then(response => response.json())
-          .then(data => {
-            NFT.image = resolveLink(data.image);
-          });
+        } else if (NFT?.token_uri) {
+          try {
+            await fetch(NFT.token_uri)
+              .then((response) => response.json())
+              .then((data) => {
+                NFT.image = resolveLink(data.image);
+              });
+          } catch (error) {
+            setFetchSuccess(false);
+          }
         }
       }
       setNFTTokenIds(NFTs);
     }
   }, [data]);
 
-  return { getNFTTokenIds, NFTTokenIds, totalNFTs, error, isLoading };
+  return {
+    getNFTTokenIds,
+    NFTTokenIds,
+    totalNFTs,
+    fetchSuccess,
+    error,
+    isLoading,
+  };
 };
